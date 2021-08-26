182d429997f42195cf310b99607e3764e04e541b
m.a.mishina <m.a.mishina@tinkoff.ru>
2021-08-26 15:44:49 +0300

    e2e tests

--- src/main/java/com/tinkoff/edu/app/loan/repository/impl/LoanCalcRepository.java
+++ src/main/java/com/tinkoff/edu/app/loan/repository/impl/LoanCalcRepository.java
@@ -3,19 +3,18 @@ package com.tinkoff.edu.app.loan.repository.impl;
 import com.tinkoff.edu.app.loan.repository.ILoanCalcRepository;
 
 public class LoanCalcRepository implements ILoanCalcRepository {
-    private static int requestId;
+    private int requestId;
 
     public int save() {
         return ++requestId;
     }
 
-    public static void setRequestId(int requestId) {
-        LoanCalcRepository.requestId = requestId;
-    }
-
-    public static int getRequestId() {
+    public int getRequestId() {
         return requestId;
     }
 
+    public void setRequestId(int requestId) {
+        this.requestId = requestId;
+    }
 
 }
--- src/test/java/com/tinkoff/edu/AppTest.java
+++ src/test/java/com/tinkoff/edu/AppTest.java
@@ -3,16 +3,16 @@ package com.tinkoff.edu;
 import com.tinkoff.edu.app.loan.controller.LoanCalcController;
 import com.tinkoff.edu.app.loan.models.LoanData;
 import com.tinkoff.edu.app.loan.models.LoanResponse;
-import com.tinkoff.edu.app.loan.repository.ILoanCalcRepository;
 import com.tinkoff.edu.app.loan.repository.impl.LoanCalcRepository;
 import com.tinkoff.edu.app.loan.service.impl.LoanCalcService;
 import com.tinkoff.edu.app.loan.types.LoanType;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
-    private ILoanCalcRepository repository;
+    private LoanCalcRepository repository;
     private LoanCalcService service;
     private LoanCalcController controller;
     private LoanData request;
@@ -26,18 +26,18 @@ public class AppTest {
     }
 
     @Test
+    @DisplayName ("requestId=1 при первом вызове")
     public void shouldGetId1WhenFirstCall() {
         LoanResponse requestId = controller.createRequest(request);
         assertEquals(1, requestId.getId());
     }
 
     @Test
+    @DisplayName ("requestId инкрементируется при повторных вызовах")
     public void shouldGetIncrementedIdWhenAnyCall() {
-        int testRequestId = LoanCalcRepository.getRequestId();
-        LoanCalcRepository.setRequestId(10);
+        repository.setRequestId(10);
         LoanResponse requestId = controller.createRequest(request);
         assertEquals(11, requestId.getId());
-        LoanCalcRepository.setRequestId(testRequestId);
     }
 
 
